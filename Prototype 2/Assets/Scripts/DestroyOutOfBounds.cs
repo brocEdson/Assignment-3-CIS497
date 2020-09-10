942e92daafae6a919475a7e5b8e97deb183d848e
brocEdson <bedson@mail.bradley.edu>
2020-09-09 21:33:03 -0500

    Added code for DestroyOutOfBounds

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿/* Broc Edson
+ * Prototype 2
+ * Destroys an object when it is out of bounds
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottomBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound || transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+            if(transform.position.z < bottomBound)
+            {
+                Debug.Log("Game Over!");
+            }
+        }
+    }
+}
