f619c1ea560171d58403918db0a6db33c72802bb
brocEdson <bedson@mail.bradley.edu>
2020-09-09 21:27:43 -0500

    Added code for DetectCollision

---
+++ Prototype 2/Assets/Scripts/DetectCollisons.cs
@@ -0,0 +1,16 @@
+﻿/* Broc Edson
+ * Prototype 2
+ * Destroys two objects when a collision occurs
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisons : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject); 
+    }
+}
