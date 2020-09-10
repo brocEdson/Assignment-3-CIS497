d5555f630b124812cf857d567c348a076754ebdb
brocEdson <bedson@mail.bradley.edu>
2020-09-09 20:59:16 -0500

    Wrote PlayerController code

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿/* Broc Edson
+ * Prototype 2
+ * Allows the player to be controlled
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float horizontalInput;
+    public float speed = 10f;
+    private float xRange = 14;
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
