5ffd7d1c2e4e874904f12533fe6f3a46075f761e
brocEdson <bedson@mail.bradley.edu>
2020-09-09 21:21:21 -0500

    Added code for ShootPrefab

---
+++ Prototype 2/Assets/Scripts/ShootPrefab.cs
@@ -0,0 +1,21 @@
+﻿/* Broc Edson
+ * Prototype 2
+ * Shoots a prefab from an object
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPrefab : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, prefab.transform.rotation);
+        }
+    }
+}
